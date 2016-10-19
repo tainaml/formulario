892cf72761ceccd3f745eb38653a2ab6ce95bbf9
tainaml <tainaml@live.ca>
2016-10-19 09:16:02 -0300

    primeiro envio de conn - ja iniciado

--- conexao.php
+++ conexao.php
@@ -1,19 +1,30 @@
 <?php
 
+$nome = $_POST["nome_solicitante"];  
+$telefone = $_POST["telefone"];    //parte 1 
+$responsavel = $_POST["nome_responsavel"];
+$emailResp = $POST["email_responsavel"]
+
+
+
+//conexao
 $servidor ='localhost';
 $usuario = 'root';
 $senha = 'root';
-$bancodedados= 'notas';
+$bancodedados= 'formulario';
+
 
 
 $conexao = mysqli_connect($servidor, $usuario, $senha, $bancodedados);
 
 if(mysqli_connect_errno()) trigger_error(mysqli_connect_error());
 
-$query = "insert into aluno (nome, disciplina) values ('$_POST[nome]','$_POST[disciplina]')";
+$query = "insert into solicitante (nome, telefone, responsavel_setor, email) values 
+		  ('$_POST[nome]','$_POST[telefone]', '$_POST[responsavel]', '$_POST[emailResp]'";
+
 
 if (!mysqli_query($conexao,$query)) {
-    
+    echo 'erro';
     die("Error: " . mysqli_error($conexao));
 }else{
     
@@ -25,3 +36,6 @@ if (!mysqli_query($conexao,$query)) {
 
 
 ?>
+
+
+
